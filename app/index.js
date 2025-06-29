4b5ed0561491217433680288e43c2754890ba663
Lukasz <lukasz.grzeszak@onet.pl>
2025-06-29 17:42:23 +0200

    dwatrzy committenssn

--- app/index.js
+++ app/index.js
@@ -5,7 +5,7 @@ const app = express();
 app.get('/', async (req, res) => {
   try {
     await db.getConnection();
-    res.send('Connected to DB!');
+    res.send('Connected to DB! <br>Zart na dzis: Jak nazywaja sie ulubione chipsy hydraulika? <br> kranczips');
   } catch (err) {
     res.status(500).send('DB connection failed');
   }
