7cbff3fcf7cd7d5188643c71a60a4ec9bdf0ada0
Lukasz <lukasz.grzeszak@onet.pl>
2025-06-30 11:30:56 +0200

    miriamlukasz

--- app/index.js
+++ app/index.js
@@ -5,11 +5,11 @@ const app = express();
 app.get('/', async (req, res) => {
   try {
     await db.getConnection();
-    res.send('Connected to DB! <br>Zart na dzis: Jak nazywaja sie ulubione chipsy hydraulika? <br> kranczips');
+    res.send('Connected to DB! <br>Zart na dzis: Jakk nazywaja sie ulubione chipsy hydraulika? <br> kranczips');
   } catch (err) {
     res.status(500).send('DB connection failed');
   }
 });
 
 app.listen(3000, () => console.log('App listening on port 3000'));
-//komentarz 37
+//komentarz 3777
